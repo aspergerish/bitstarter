8667d8a23202f13d43f403756b5ed4d4755dc3aa
aspergerish <aspergerish@gmail.com>
2013-07-09 13:53:50 +0000

    url enabled grader.js

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require("restler");
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,20 +56,34 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+var checkFile = function(f, c) {
+    var checkJson = checkHtmlFile(f, c);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url !== undefined) {
+        rest.get(program.url).on('complete', function(result) {
+            fs.writeFile("tempfile.txt", result, function(err) {
+                if (err) throw err;
+                checkFile("tempfile.txt", program.checks);
+            });
+        });
+    } else {
+        checkFile(program.file, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
